34a30bdeb52bdb2776970e96c328046a1a261524
Soikat <193371447+soikat27@users.noreply.github.com>
2026-06-05 15:31:57 -0400

    Implement: toggle between idle and ready view

--- src/modules/ui-controller.js
+++ src/modules/ui-controller.js
@@ -1,20 +1,47 @@
+import AppController from "./app-controller.js"
+
 const uiController = (() => {
     function fetchWeather(event) {
         event.preventDefault();
 
         const form = event.currentTarget;
         // validate form-fields
-        validateLocation();
+        validateLocationIdle();
         if (!form.checkValidity()) {
             form.reportValidity();
             return;
         }
         
-        // const location = document.getElementById("location-input-idle").value;
+        const location = document.getElementById("location-input-idle").value;
+        // try {
+        //     const data = AppController.fetchWeather(location);
+        //     const weather = AppController.processWeatherData(data);
+        // }
+        // catch(error) {
+
+        // }
 
     }
 
-    function validateLocation() {
+    function showIdleView() {
+        const idleSection = document.getElementById("app-idle");
+        const readySection = document.getElementById("app-ready");
+        readySection.classList.add("hide");
+        idleSection.classList.remove("hide");
+    }
+
+    function showReadyView() {
+        const idleSection = document.getElementById("app-idle");
+        const readySection = document.getElementById("app-ready");
+        idleSection.classList.add("hide");
+        readySection.classList.remove("hide");
+    }
+
+    // function developMainSection() {
+
+    // }
+
+    function validateLocationIdle() {
         // 1. reset custom validity
         const searchBar = document.getElementById("location-input-idle");
         searchBar.setCustomValidity("");
@@ -29,7 +56,9 @@ const uiController = (() => {
         const idleForm = document.getElementById("search-form-idle");
         idleForm.addEventListener("submit", fetchWeather);
         const idleSearchBar = document.getElementById("location-input-idle");
-        idleSearchBar.addEventListener("input", validateLocation);
+        idleSearchBar.addEventListener("input", validateLocationIdle);
+
+
     }
 
     function initApp() {

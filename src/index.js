78ca12efe5fd675aa518f370b4c036dea50de7ed
Soikat <193371447+soikat27@users.noreply.github.com>
2026-06-05 15:07:09 -0400

    Set custom validity to searchbar and add event listener to idle-form
    -- upon empty location: show custom error!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import "./styles/normalize.css";
 import "./styles/style.css";
 
 import AppController from "./modules/app-controller.js";
+import UiController from "./modules/ui-controller.js";
 
 const promise = AppController.fetchWeather("gettysburg");
 AppController.processWeatherData(promise);
+
+UiController.initApp();
--- src/modules/ui-controller.js
+++ src/modules/ui-controller.js
@@ -1,11 +1,42 @@
 const uiController = (() => {
-    function setEventListeners() {
+    function fetchWeather(event) {
+        event.preventDefault();
 
+        const form = event.currentTarget;
+        // validate form-fields
+        validateLocation();
+        if (!form.checkValidity()) {
+            form.reportValidity();
+            return;
+        }
+        
+        // const location = document.getElementById("location-input-idle").value;
+
+    }
+
+    function validateLocation() {
+        // 1. reset custom validity
+        const searchBar = document.getElementById("location-input-idle");
+        searchBar.setCustomValidity("");
+
+        // 2. check validity and set custom validity
+        if (!searchBar.value.trim())
+            searchBar.setCustomValidity("Location can't be empty!");
+    }
+    
+    function setEventListeners() {
+        // idle view: searchbar, custom validity 
+        const idleForm = document.getElementById("search-form-idle");
+        idleForm.addEventListener("submit", fetchWeather);
+        const idleSearchBar = document.getElementById("location-input-idle");
+        idleSearchBar.addEventListener("input", validateLocation);
     }
 
     function initApp() {
-        
+        setEventListeners();
     }
+
+    return {initApp};
 })();
 
 export default uiController;
